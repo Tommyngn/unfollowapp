baa0228839d40a6fc506e783f5741d42e7a2c96a
Tommy Nguyen <tommynguyen@huy-nguyens-mbp.attlocal.net>
2020-12-13 15:38:09 -0600

    jkl

--- jobs.py
+++ jobs.py
@@ -1,17 +1,18 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
 from instafollowers import  unfollowbot
 import csv
-import config
 from datetime import datetime
 
 
 sched=BlockingScheduler(daemon=True)
+names=[]
 
 @sched.scheduled_job('interval', minutes=5, args=['noahthemac'],next_run_time=datetime.now())
 def timed_job(name):
     print('This job is run every three minutes.')
-    config.names.append('tommm')
-    print(config.names)
+    global names
+    names.append('hiii')
+    print(names)
     # bot = unfollowbot()
     # l = bot.getfollowerlist(name)
     # f = open('following_list_updated_new.csv', 'r')
