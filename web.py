daa85cace0f7a49698afbec2eafdd235f6ce7cc7
Tommy Nguyen <tommynguyen@huy-nguyens-mbp.attlocal.net>
2020-12-13 16:04:23 -0600

    sdfg

--- web.py
+++ web.py
@@ -2,39 +2,41 @@ from instafollowers import unfollowbot
 from flask import Flask , render_template
 import config
 from apscheduler.schedulers.blocking import BlockingScheduler
-from jobs import timed_job
+# from jobs import timed_job
 import csv
 import time
 from datetime import datetime
 
 app=Flask(__name__,template_folder='templatess')
 sched=BlockingScheduler(daemon=True)
-names=[]
+namess=[]
 
-# def timed_job(name):
-#     print('This job is run every three minutes.')
-#     global count
-#     global names
-#     names.clear()
-#     count+=1
-#     names.append(str(count))
-#     bot = unfollowbot()
-#     l = bot.getfollowerlist(name)
-#     f = open('following_list_updated_new.csv', 'r')
-#     g = list(csv.reader(f))
-#     h=[]
-#     for i in g:
-#         h.append(i[0])
-#     for i in l:
-#         if str(i) not in h:
-#             names.append(i)
+def timed_job(name):
+    print('This job is run every three minutes.')
+    # global count
+    global namess
+    namess.append('hiii')
+    print(namess,' yess')
+    # names.clear()
+    # count+=1
+    # names.append(str(count))
+    # bot = unfollowbot()
+    # l = bot.getfollowerlist(name)
+    # f = open('following_list_updated_new.csv', 'r')
+    # g = list(csv.reader(f))
+    # h=[]
+    # for i in g:
+    #     h.append(i[0])
+    # for i in l:
+    #     if str(i) not in h:
+    #         names.append(i)
 
 sched.add_job(timed_job,'interval', minutes=5, args=['noahthemac'], next_run_time=datetime.now() )
 
 @app.route('/')
 def home():
 
-    return render_template('index.html',names=names)
+    return render_template('index.html',names=namess)
 
 
 
