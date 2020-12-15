fb3fe07850c32facb48518753d355725f7ca606b
Tommy Nguyen <tommynguyen@huy-nguyens-mbp.attlocal.net>
2020-12-15 15:40:13 -0600

    kl

--- instafollowers.py
+++ instafollowers.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from multiprocessing import Process
 import time
 import csv
+import os
 
 
 class unfollowbot:
@@ -15,6 +16,14 @@ class unfollowbot:
     #     chrome_option.add_argument('--headless')
     #     chrome_option.add_argument('--window-size=1920x1080')
     #     chrome_option.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36')
+
+        # chrome_options = webdriver.ChromeOptions()
+        # chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        # chrome_options.add_argument("--headless")
+        # chrome_options.add_argument("--disable-dev-shm-usage")
+        # chrome_options.add_argument("--no-sandbox")
+        #
+        # drive = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     #
     #     drive = webdriver.Chrome(options=chrome_option, executable_path=path)
     #     drive.get('https://instagram.com')
