9be91ddfe11f872d3deb12ccd93e2c3c18536390
Tommy Nguyen <tommynguyen@huy-nguyens-mbp.attlocal.net>
2020-12-29 17:49:05 -0600

    po

--- web.py
+++ web.py
@@ -39,6 +39,7 @@ def refresh():
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
+    chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument("--disable-dev-shm-usage")
     drive = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     drive.get('https://unfollow-app.herokuapp.com/')
