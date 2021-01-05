5770af9cdd47297c213992a5daf896665b6eb19d
Tommy Nguyen <tommynguyen@huy-nguyens-mbp.attlocal.net>
2021-01-04 18:03:32 -0600

    l

--- instafollowers.py
+++ instafollowers.py
@@ -109,9 +109,9 @@ class unfollowbot:
         # time.sleep(500000)
 
         time.sleep(6)
-        page=drive.find_element_by_xpath("//button[contains(text(), 'Not Now')]")
-        page.click()
-        time.sleep(6)
+        # page=drive.find_element_by_xpath("//button[contains(text(), 'Not Now')]")
+        # page.click()
+        # time.sleep(6)
         # page1=drive.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
         # page1.click()
         # time.sleep(3)
